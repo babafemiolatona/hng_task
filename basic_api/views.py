97b85b58dd367e43a93b9f29bafed6e5699637ff
hgfemi <femiolatona@gmail.com>
2022-10-27 16:37:08 +0200

    First Commit

--- basic_api/views.py
+++ basic_api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view('GET')
 def info_list(request):
     """
     List all code snippets, or create a new snippet.
