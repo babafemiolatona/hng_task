f1f13c40a2bb86e5cf46ab77c30e186c2c063f23
hgfemi <femiolatona@gmail.com>
2022-10-27 10:41:51 +0200

    First Commit

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
