3bcc41e9eb307f9002118247e43f2d778b237a65
hgfemi <femiolatona@gmail.com>
2022-10-27 23:22:03 +0200

    Updated Views

--- basic_api/views.py
+++ basic_api/views.py
@@ -1,15 +1,10 @@
 from .models import Info
 from .serializers import InfoSerializer
-from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view('GET')
+@api_view(['GET'])
 def info_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        info = Info.objects.all()
-        serializer = InfoSerializer(info, many=True)
-        return Response(serializer.data)
+    info = Info.objects.get()
+    serializer = InfoSerializer(info)
+    return Response(serializer.data)
