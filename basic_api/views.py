949095ec961252b7b0bfbf8b172788e2c21c0505
hgfemi <femiolatona@gmail.com>
2022-10-28 10:26:45 +0200

    Updated Views

--- basic_api/views.py
+++ basic_api/views.py
@@ -1,10 +1,13 @@
-from .models import Info
-from .serializers import InfoSerializer
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
-@api_view(['GET'])
-def info_list(request):
-    info = Info.objects.get()
-    serializer = InfoSerializer(info)
-    return Response(serializer.data)
+@api_view(["GET"])
+def home(request, *args, **kwargs):
+    header = {"Access-Control-Allow-Origin":"*"}
+    info = {
+        "slackUsername":"outsider",
+        "backend":True,
+        "age": 27,
+        "bio":"I'm a backend developer"
+  }
+    return JsonResponse(info, headers=header)
